295ad125b4a346fca6c1cc8cb397db74296e3542
amiroo54 <amiroo.f54@gmail.com>
2023-11-07 16:32:07 +0330

    reworked header, fixed some bugs.

--- scripts/header.js
+++ scripts/header.js
@@ -1,55 +1,35 @@
 var headerDiv = document.querySelector(".header");
 
-const HeaderString = 
-'<header><h1>Amiroof\'s very Professional Website</h1></header>' +
-'<nav>' +
-'    <ul class="nav">' +
-'        <li><a href="index.html">Home</a></li>' +
-'        <li><a href="Projects.html">Projects</a></li>' +
-'        <li><a href="#">Articles</a></li>' +
-'    </ul>' +
-'</nav>' ;
+const xhr = new XMLHttpRequest();
+xhr.open('GET', 'Header.html', false); 
+xhr.send();
 
-/*
-const input = document.getElementById("file-sel");
-
-input.addEventListener('change', handleFiles, false);
-
-function handleFiles()
+if (xhr.status == 200)
 {
-    var fr = new FileReader(); 
-    fr.onload=function()
-    { 
-        console.debug(fr.result);
-        loadHeader(fr.result);
-    } 
-
-    fr.readAsText(this.files[0]);
+  loadHeader(xhr.responseText);
 }
-*/
-loadHeader(HeaderString);
+
 function loadHeader(HeaderString)
 {
-    const parser = new DOMParser();
-    const hDoc = parser.parseFromString(HeaderString, "text/html"); //getting a DOM object from the string.
-    
+  const parser = new DOMParser();
+  const hDoc = parser.parseFromString(HeaderString, "text/html"); //getting a DOM object from the string.
+  
 
-    var navs = hDoc.querySelectorAll(".nav a");
-    var currentPage = window.location.href.split("/").pop().replace("#", ""); //getting current page url.
-    console.debug(currentPage);
-    for (var i = 0; i < navs.length; i++) 
-    {
-        if (navs[i].getAttribute("href") === currentPage) 
-        { 
-            //changing attributes in the selected nav.
-            navs[i].classList.add("current");
-            navs[i].setAttribute("href", "#");
-        }
+  var navs = hDoc.querySelectorAll(".nav a");
+  var currentPage = window.location.href.split("/").pop().replace("#", ""); //getting current page url.
+  for (var i = 0; i < navs.length; i++) 
+  {
+    console.log(currentPage);
+    if (navs[i].getAttribute("href") === currentPage || (navs[i].getAttribute("href") == "index.html" && currentPage==""))
+    { 
+      navs[i].classList.add("current");
+      navs[i].setAttribute("href", "#");
     }
-    
-    const serializer = new XMLSerializer();
-    
-    HeaderString = serializer.serializeToString(hDoc);
-    headerDiv.innerHTML = HeaderString; //adding the modified header to the selected div.
+  }
+  
+  const serializer = new XMLSerializer();
+  
+  HeaderString = serializer.serializeToString(hDoc);
+  headerDiv.innerHTML = HeaderString; //adding the modified header to the selected div.
 }
 
---
+++ scripts/theme.js
@@ -0,0 +1,51 @@
+changeTheme();
+let b = document.getElementById("themeChange");
+b.onchange = onThemeChange;
+
+if (localStorage.getItem('theme') == "dark")
+{
+    b.checked = true;
+}else
+{
+    b.checked = false;
+}
+
+function onThemeChange()
+{
+    if (this.checked)
+    {
+        localStorage.setItem("theme", "dark");
+        changeTheme();
+    }else
+    {
+        localStorage.setItem("theme", "light");
+        changeTheme();
+    }
+}
+
+function changeTheme()
+{
+
+    let currentTheme = localStorage.getItem("theme");
+    var icon = document.querySelector('[rel="icon"');
+
+    if (currentTheme == "dark")
+    {
+        //change everything to dark.
+        icon.setAttribute("href", "resources/Icons/Icon-Light-512.png");
+        document.documentElement.style.setProperty('--w', "#051923");
+        document.documentElement.style.setProperty('--lb', "#003554");
+        document.documentElement.style.setProperty('--b', "#006494");
+        document.documentElement.style.setProperty('--mb', "#1582ca");
+        document.documentElement.style.setProperty('--db', "#bfdfff");
+    } else if(currentTheme == "light")
+    {
+        //change everything to light.
+        icon.setAttribute("href", "resources/Icons/Icon-Dark-512.png");
+        document.documentElement.style.setProperty('--db', "#051923");
+        document.documentElement.style.setProperty('--mb', "#003554");
+        document.documentElement.style.setProperty('--b', "#006494");
+        document.documentElement.style.setProperty('--lb', "#1582ca");
+        document.documentElement.style.setProperty('--w', "#bfdfff");
+    }
+}
