acc1b855ff07718753bcb65d61f6a2b0fbc74ef8
amiroof54 <amiroo.f54@gmail.com>
2024-03-03 12:14:16 +0330

    test.

--- Tools/SteamCalculator/steamCalculator.js
+++ Tools/SteamCalculator/steamCalculator.js
@@ -4,7 +4,7 @@ function getKeyPrice()
     const TF2AppId = 440;
     const MannCoSupplyCrateKeyHashName = "Mann Co. Supply Crate Key"
     const Key = "C769E88431D6F887F8EDD34FBF63949A";
-    const SteamURL = `http://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001/?key=${Key}&appid=${TF2AppId}`
+    const SteamURL = `https://api.steampowered.com/ISteamEconomy/GetAssetPrices/v0001/?key=${Key}&appid=${TF2AppId}`;
     fetch(SteamURL).then((res) => res.json()).then((res) =>
     {
         console.log(res);
